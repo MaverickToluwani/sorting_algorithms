ffd56bf238884f5ef52f2cc2c4b6352a60c8d455
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-05-01 10:08:41 -0700

    bubble sort file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array from ascending order
+ * @array: Array tobe sorted
+ * @size: lenght of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, idx;
+	int swp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = swp;
+				for (idx = 0; idx < size; idx++)
+				{
+					if (idx == size - 1)
+						printf("%d", array[idx]);
+					else
+						printf("%d, ", array[idx]);
+				}
+				printf("\n");
+			}
+		}
+	}
+}
